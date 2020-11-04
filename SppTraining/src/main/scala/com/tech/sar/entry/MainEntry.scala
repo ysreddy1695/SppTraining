0d8fbe9ed18494d1f449540559922ddc0b11c27c
SarathY <ysreddy1695@gmail.com>
2020-11-04 20:33:50 +0530

    First Commit

---
+++ SppTraining/src/main/scala/com/tech/sar/entry/MainEntry.scala
@@ -0,0 +1,28 @@
+package com.tech.sar.entry
+
+import com.tech.sar.session.DemoSparkSession
+
+object MainEntry {
+  
+  def main(args: Array[String]): Unit = {
+    
+    println("Starting the entry point --------------->")
+    
+    val spark = DemoSparkSession.spark
+    import spark.implicits._
+    
+    // Employee Data
+    val empDF = Seq((8, "John" , 1),(64, "Mike", 2), (27, "Garner", 1)).toDF("EmpId", "EmpName" , "DepId")
+    empDF.show()
+    
+    // Department Data
+    val depDF = Seq((1,"IT"),(2,"ACCOUNTS")).toDF("DepId" , "DepName")
+    depDF.show()
+    
+    // Joined Data
+    val resultant = empDF.join(depDF, "DepId").select($"EmpName", $"DepName")
+    resultant.show()
+    
+    println("Finishing the entry point --------------->")
+  }
+}
